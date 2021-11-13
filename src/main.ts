53236bbf11c322afa25cf3f35cad688db0b819c7
misteltein <itogahibiki@gmail.com>
2021-11-13 15:51:40 +0900

    p => p5 にリネーム

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import P5 from "p5";
 
-const sketch = (p: P5) => {
-  p.setup = () => {
-    p.createCanvas(300, 300);
-    p.background(51);
-  };
+const sketch = (p5: P5) => {
+    p5.setup = () => {
+        p5.createCanvas(300, 300);
+        p5.background(51);
+    };
 };
 
 /* eslint-disable no-new */
